9004ee0ba88a45342b31f726e0f4a859ce1e9307
Vlad Smyslov <vladisl.smislow2010@yandex.ru>
2024-06-07 00:52:57 +0300

    initial commit

---
+++ blogicum/blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    # Напишите адрес тут
+    path('', views.index, name='index'),
+    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<slug:category_slug>/', views.category_posts, name='category_posts'),
+]
---
+++ blogicum/pages/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+def about(request):
+    template_name = 'pages/about.html'
+    return render(request, template_name)
+
+
+def rules(request):
+    template_name = 'pages/rules.html'
+    return render(request, template_name)
+# Create your views here.
